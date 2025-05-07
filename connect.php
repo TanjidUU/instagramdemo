0871552e616befb2cc23334bb464a3438e8e5c0c
TanjidUU <tanjidmasum6@gmail.com>
2025-05-07 12:38:07 +0100

    init

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
 
-	$servername = 'localhost';
-	$user 		= 'root';
-	$pass 		= '';
+	$servername = 'instademo.mysql.database.azure.com';
+	$user 		= 'Tanjid';
+	$pass 		= '113601tanjid*';
 	$db 		= 'insta_db';
 	$conn 		= mysqli_connect($servername, $user, $pass, $db);
 	
